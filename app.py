8d08c9d9898c86ef97342b26a69752ecf43cec6f
Tony Cao <112421141+Tony2K3@users.noreply.github.com>
2025-11-01 19:06:12 -0700

    Upload

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
-
-
 from flask import Flask
+
 
 app = Flask(__name__)
 
-@app.route("/")
 
+@app.route("/")
 def home():
-    return "<h1>🚀 Hello from Flask CI/CD Demo v1!</h1><p>Automatic Build → Test → Deploy via GitHub Actions.</p>"
+    message = (
+        "<h1>🚀 Hello from Flask CI/CD Demo v1!</h1>"
+        "<p>Automatic Build → Test → Deploy via GitHub Actions.</p>"
+    )
+    return message
+
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
